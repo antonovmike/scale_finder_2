9c17ac3314cc1815a164679832b2e55a66e04723
antonovmike <antonvma@outlook.com>
2023-03-03 21:21:58 +0500

    split and unite scale

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 mod scales;
 use crate::scales::*;
 
-fn main() {}
+fn main() {
+	// ARRAY TO VEC
+	let array: [u8; 3] = [9, 8, 7];
+	let a = array.to_vec();
+	println!("{:?}\n{:?}", array, a);
+	
+	// SPLIT ARRAY
+	let split_here = 3;
+	let array_2: [u8; 8] = [2, 2, 1, 2, 2, 2, 1, 2];
+	let (first, second) = array_2.split_at(split_here);
+	println!("first {first:?}\nsecond{second:?}");
+
+	// COMBINE NEW SCALE AS A VEC
+	let mut vec_a: Vec<u8> = vec![];
+	for i in second {
+		vec_a.push(*i)
+	}
+	for i in first {
+		vec_a.push(*i)
+	}
+	println!("Parts united: {:?}", vec_a);
+
+	// VEC TO ARRAY
+	let vektor: Vec<u8> = vec![1,2,3,4,5];
+	let b: [u8; 5] = vektor.try_into().unwrap();
+	println!("{:?}", b);
+}
 
 fn scale_finder(note_sequence: [char; 8], note_steps: [u8; 8], scale: [u8; 8]) -> String {
 	let mut empty_string = "".to_string();
