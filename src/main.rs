618d7b583954e2ba2e5f5f2976a3f492f01e20ee
antonovmike <antonvma@outlook.com>
2023-03-03 18:41:29 +0500

    it works

--- src/main.rs
+++ src/main.rs
@@ -2,106 +2,111 @@ mod scales;
 use crate::scales::*;
 
 #[allow(unused)]
+const NOTES: [char; 8] = ['C', 'D', 'E', 'F', 'G', 'A', 'H', 'C'];
+#[allow(unused)]
+const STEPS: [u8; 8] = [2, 2, 1, 2, 2, 2, 1, 2];
 
 fn main() {
-    modes('F', PHRYGIAN);
+	let a = scale_finder(NOTES, STEPS, MINOR_AEOLIAN);
+    println!("{a}");
+    println!("Minor: C -- D - Eb -- F -- G - Ab - Hb -- C");
 }
 
-fn modes(current_note: char, current_mode: [u8; 8]) -> String {
-	let notes = [ 'C', 'D', 'E', 'F', 'G', 'A', 'H' ];
-	let notes_tuples = [ ('C', 2), ('D', 2), ( 'E', 1), ('F', 2), ('G', 2), ('A', 2), ('H', 1)];
-
-	let index = notes.iter().position(|&r| r == current_note).unwrap();
-    let mut index_a = index;
-	let mut index_mode = 0;
-	let mut mode_sharps = "".to_string();
-	let mut mode_flats  = "".to_string();
-
-	for i in index_a..notes.len() {
-		println!("{}. Distance = {}\tCurrent note is {} and it's lenght = {}", 
-			index_mode + 1, current_mode[index_mode], notes_tuples[index_a].0, notes_tuples[index_a].1
-		);
-		mode_sharps.insert(mode_sharps.len(), notes_tuples[index_a].0);
-		mode_flats.insert(mode_flats.len(), notes_tuples[index_a].0);
-		if i > 0 {
-			if notes_tuples[index_a].1 < current_mode[index_mode] {
-				mode_flats.insert(mode_flats.len(), 'b');
-				println!("This note {}/{} should be FLAT", i, notes_tuples[index_a].0)
-			}
-			if notes_tuples[index_a].1 > current_mode[index_mode] {
-				mode_sharps.insert(mode_sharps.len(), '#');
-				println!("This note {}/{} should be SHARP", i, notes_tuples[index_a].0)
-			}
-		}
-
-		index_a += 1;
-		index_mode += 1;
-	}
-	if index > 0 && index < 7 {
-		for i in 0..index {
-			println!("{}. DISTANCE = {}\tCurrent note is {} and it's lenght = {}", 
-				index_mode + 1, current_mode[index_mode], notes_tuples[i].0, notes_tuples[index].1
-			);
-			mode_sharps.insert(mode_sharps.len(), notes_tuples[i].0);
-			mode_flats.insert(mode_flats.len(), notes_tuples[i].0);
-			if notes_tuples[i].1 < current_mode[index_mode] {
-				mode_flats.insert(mode_flats.len(), 'b');
-				println!("This note {}/{} should be FLAT", i, notes_tuples[i].0)
-			}
-			if notes_tuples[i].1 > current_mode[index_mode] {
-				mode_sharps.insert(mode_sharps.len(), '#');
-				println!("This note {}/{} should be SHARP", i, notes_tuples[i].0)
-			}
-		}
-		index_a += 1;
-		index_mode += 1;
-	}
+fn scale_finder(note_sequence: [char; 8], note_steps: [u8; 8], scale: [u8; 8]) -> String {
+	let mut empty_string = "".to_string();
+	let mut index = 0;
+	let mut shift_up = false;
+	let mut shift_down = false;
 	
-	let sharp_vec_of_chars: Vec<char> = mode_sharps.chars().collect();
-    let flat_vec_of_chars:  Vec<char> = mode_flats.chars().collect();
-	let mut sharp_vec_of_u8: Vec<u8> = vec![];
-	let mut flat_vec_of_u8: Vec<u8> = vec![];
-	for s in 0..sharp_vec_of_chars.len() {
+	for i in note_steps {
+		if i == scale[index] {
+			if !shift_down && !shift_up {
+				empty_string = format!("{}{}", empty_string, note_sequence[index]);
+			}
+			if shift_down {
+				empty_string = format!("{}{}{}", empty_string, note_sequence[index], 'b');
+			}
+			if shift_up {
+				empty_string = format!("{}{}{}", empty_string, note_sequence[index], '#');
+			}
+		}
+		else if i < scale[index] && !shift_up && !shift_down {
+			empty_string = format!("{}{}", empty_string, note_sequence[index]);
+			shift_up = true
+		}
+		else if i > scale[index] && !shift_up && !shift_down {
+			empty_string = format!("{}{}", empty_string, note_sequence[index]);
+			shift_down = true
+		}
+		else if i < scale[index] && shift_up && !shift_down {
+			empty_string = format!("{}{}{}", empty_string, note_sequence[index], '#');
+		}
+		else if i > scale[index] && shift_up && !shift_down {
+			empty_string = format!("{}{}{}", empty_string, note_sequence[index], '#');
+			shift_up = false
+		}
 		
+		else if i > scale[index] && shift_down && !shift_up {
+			empty_string = format!("{}{}{}", empty_string, note_sequence[index], 'b');
+		}
+		else if i < scale[index] && shift_down && !shift_up {
+			empty_string = format!("{}{}{}", empty_string, note_sequence[index], 'b');
+			shift_down = false
+		}	
+		index += 1
 	}
-	
-	println!("SHARPS {}", mode_sharps);
-	println!("FLATS  {}", mode_flats);
-
-	mode_flats
+	empty_string
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn phrygian_c() {
-		assert_eq!(("CDbEbFGAbHb".to_string()), modes('C', PHRYGIAN))
+	#[test]
+	fn c_minor() {
+		assert_eq!("CDEbFGAbHbC".to_string(), scale_finder(NOTES, STEPS, MINOR_AEOLIAN))
 	}
-    #[test]
-    fn phrygian_d() {
-		assert_eq!(("DEbFGAHbC".to_string()), modes('D', PHRYGIAN))
+	#[test]
+	fn d_minor() {
+	 let d: [char; 8] = ['D', 'E', 'F', 'G', 'A', 'H', 'C', 'D'];
+	 let d_steps: [u8; 8] = [2, 1, 2, 2, 2, 1, 2, 2];
+	 assert_eq!("DEFGAHbCD".to_string(), scale_finder(d, d_steps, MINOR_AEOLIAN))
 	}
-    #[test]
-    fn phrygian_e() {
-		assert_eq!(("EFGAHCD".to_string()), modes('E', PHRYGIAN))
+	#[test]
+	fn e_minor() {
+	 let e: [char; 8] = ['E', 'F', 'G', 'A', 'H', 'C', 'D', 'E'];
+	 let e_steps: [u8; 8] = [1, 2, 2, 2, 1, 2, 2, 2];
+	 assert_eq!("EF#GAHCDE".to_string(), scale_finder(e, e_steps, MINOR_AEOLIAN))
 	}
-    // #[test]
-    // fn phrygian_f() {
-	// 	assert_eq!(("FGbAbHbCDbEb".to_string()), modes('F', phrygian))
-	// }
-    // #[test]
-    // fn phrygian_g() {
-	// 	assert_eq!(("GAbHbCDEbF".to_string()), modes('G', phrygian))
-	// }
-    // #[test]
-    // fn phrygian_a() {
-	// 	assert_eq!(("AHbCDEFG".to_string()), modes('A', phrygian))
-	// }
-    // #[test]
-    // fn phrygian_h() {
-	// 	assert_eq!(("HCDEF#GA".to_string()), modes('A', phrygian))
-	// }
+	#[test]
+	fn f_minor() {
+	 let f: [char; 8] = ['F', 'G', 'A', 'H', 'C', 'D', 'E', 'F'];
+	 let f_steps: [u8; 8] = [2, 2, 2, 1, 2, 2, 1, 2];
+	 assert_eq!("FGAbHbCDbEbF".to_string(), scale_finder(f, f_steps, MINOR_AEOLIAN))
+	}
+	#[test]
+	fn g_minor() {
+	 let g: [char; 8] = ['G', 'A', 'H', 'C', 'D', 'E', 'F', 'G'];
+	 let g_steps: [u8; 8] = [2, 2, 1, 2, 2, 1, 2, 2];
+	 assert_eq!("GAHbCDEbFG".to_string(), scale_finder(g, g_steps, MINOR_AEOLIAN))
+	}
+	#[test]
+	fn a_minor() {
+		let a: [char; 8] = ['A', 'H', 'C', 'D', 'E', 'F', 'G', 'A'];
+		let a_steps: [u8; 8] = [2, 1, 2, 2, 1, 2, 2, 2];
+		assert_eq!("AHCDEFGA".to_string(), scale_finder(a, a_steps, MINOR_AEOLIAN))
+	}
+	#[test]
+	fn h_minor() {
+		let h: [char; 8] = ['H', 'C', 'D', 'E', 'F', 'G', 'A', 'H'];
+		let h_steps: [u8; 8] = [1, 2, 2, 1, 2, 2, 2, 2];
+		assert_eq!("HC#DEF#GAH".to_string(), scale_finder(h, h_steps, MINOR_AEOLIAN))
+	}
+	#[test]
+	fn c_major() {
+		let c_maj: [char; 8] = ['C', 'D', 'E', 'F', 'G', 'A', 'H', 'C'];
+		let c_steps: [u8; 8] = [2, 2, 1, 2, 2, 2, 1, 2];
+		assert_eq!("CDEFGAHC".to_string(), scale_finder(c_maj, c_steps, MAJOR_IONIAN))
+	}
 }
--- src/scales.rs
+++ src/scales.rs
@@ -3,5 +3,5 @@ pub const DORIAN:        [u8; 8] = [2, 1, 2, 2, 2, 1, 2, 2];
 pub const PHRYGIAN:      [u8; 8] = [1, 2, 2, 2, 1, 2, 2, 2];
 pub const LYDIAN:        [u8; 8] = [2, 2, 2, 1, 2, 2, 2, 1];
 pub const MIXOLYDIAN:    [u8; 8] = [2, 2, 1, 2, 2, 2, 1, 2];
-pub const MINOR_AEOLIAN: [u8; 8] = [2, 1, 2, 2, 2, 1, 2, 2];
+pub const MINOR_AEOLIAN: [u8; 8] = [2, 1, 2, 2, 1, 2, 2, 2];
 pub const LOCRIAN:       [u8; 8] = [1, 2, 2, 2, 1, 2, 2, 2];
