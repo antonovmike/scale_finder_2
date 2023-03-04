200fba8ea337821a4e335584f6d8978c70903aa0
antonovmike <antonvma@outlook.com>
2023-03-04 20:45:58 +0500

    fmt main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,14 @@ mod scales;
 use crate::diatonic::scale_finder;
 
 fn main() {
-	// ARRAY TO VEC
-	// let array: [u8; 3] = [9, 8, 7];
-	// let a = array.to_vec();
-	// println!("{:?}\n{:?}", array, a);
-
-	// VEC TO ARRAY
-	// let vektor: Vec<u8> = vec![1,2,3,4,5];
-	// let b: [u8; 5] = vektor.try_into().unwrap();
-	// println!("{:?}", b);
-	
+	let note = 'C';
+	let modes = ["major", "ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "minor", "locrian"];
 	println!("Step number:\t1\t2\t3\t4\t5\t6\t7\t1");
-	println!("MAJOR_IONIAN: \t{}", scale_finder('C', ' ', "major"));
-	println!("DORIAN: \t{}", scale_finder('C', ' ', "dorian"));
-	println!("PHRYGIAN: \t{}", scale_finder('C', ' ', "phrygian"));
-	println!("LYDIAN: \t{}", scale_finder('C', ' ', "lydian"));
-	println!("MIXOLYDIAN: \t{}", scale_finder('C', ' ', "mixolydian"));
-	println!("MINOR_AEOLIAN: \t{}", scale_finder('C', ' ', "aeolian"));
-	println!("LOCRIAN: \t{}", scale_finder('C', ' ', "locrian"));
+	for i in modes {
+		if i == "major" || i == "minor" {
+			println!("{i}: \t\t{}", scale_finder(note, ' ', i));
+		} else {
+			println!("{i}: \t{}", scale_finder(note, ' ', i));
+		}
+	}
 }
